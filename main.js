3482630be2250421cbeabca90f8f5cd9785c789b
binohub <yeebino001@gmail.com>
2022-06-28 15:24:45 +0200

    ms0 - arrays - created

---
+++ main.js
@@ -0,0 +1,36 @@
+const teamMembers = [
+    {
+        nome: `Wayne Barnett`,
+        ruolo: `Chief Editor`,
+        "img-src": `wayne-barnett-founder-ceo.jpg`,
+    },
+    {
+        nome: `Angela Caroll`,
+        ruolo: `Founder & CEO`,
+        "img-src": `angela-caroll-chief-editor.jpg`,
+    },
+    {
+        nome: `Walter Gordon`,
+        ruolo: `Office Manager`,
+        "img-src": `walter-gordon-office-manager.jpg`,
+    },
+    {
+        nome: `Angela Lopez`,
+        ruolo: `Social Media Manager`,
+        "img-src": `angela-lopez-social-media-manager.jpg`,
+    },
+    {
+        nome: `Scott Estrada`,
+        ruolo: `Developer`,
+        "img-src": `scott-estrada-developer.jpg`,
+    },
+    {
+        nome: `Barbara Ramos`,
+        ruolo: `Graphic Designer`,
+        "img-src": `barbara-ramos-graphic-designer.jpg`,
+    },
+];
+
+for (let i=0; i<teamMembers.length; i++){
+    console.log(teamMembers[i]);
+}
